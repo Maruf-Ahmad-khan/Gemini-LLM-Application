4add494bb1584bf01e8b6995d6c9af0a1367948c
Maruf khan <mk7446683@gmail.com>
2025-01-29 18:13:57 +0530

    Creating POST Request

--- Fast_Api/main.py
+++ Fast_Api/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
-
+import os
 app = FastAPI()
 
-@app.get("/")
-async def root():
-     return {"message": "Hello, world ! "}
+# @app.get("/")
+# async def root():
+#      return {"message": "Hello, world ! ",
+#              "Name": "Maruf",
+#              "Title": "Khan",
+#              }
+
+
+
